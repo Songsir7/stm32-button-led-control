b63b128f532caaae3fff8d76987942f681c8bd2c
Song jingshi <128143618+Songsir7@users.noreply.github.com>
2026-05-22 10:22:26 +0800

    更新并加入光敏电阻与 LED 按键控制逻辑
    覆盖更新 User/main.c 文件，在原有按键控制 LED 的基础上，加入光敏电阻相关控制逻辑。
    本次修改用于 v3.0-ldr-led-button-control 分支，实现后续通过光敏电阻检测环境光状态，并结合按键输入控制 LED 的功能。

--- User/main.c
+++ User/main.c
@@ -1,36 +1,28 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"					   // Delay header
 #include "LED.h"					   // LED header
-#include "buzzer.h"				   // Buzzer header
-#include "KEY.h"					   // Key header
-
+#include "buzzer.h"				   	  // Buzzer header
+#include "KEY.h"					  // Key header
+#include "Light_Sensor.h"			  // Light Sensor header
 
 int main(void)
 {
 	LED_Init();
-	Buzzer_Init();
+	//Buzzer_Init();
 	Key_Init();
+	Light_Sensor_Init();
 	uint8_t KeyNum = 0;
 	while(1)
 	{
 		KeyNum=Key_GetNum();
-		//eyNum==0x01写法是不对的，0x01和0x03都满足条件，所以要用位运算的方式来判断按键状态
-		if(KeyNum & 0x01)
+		if(KeyNum & 0x01||Light_Sensor_GetState()==1)
 		{
-			LEDA0_ON();
-		}
-		else
-		{
-			LEDA0_OFF();
+			LEDA0_TURN();
 		}
 		if (KeyNum & 0x02)
 		{
-			LEDC14_ON();
+			LEDC14_TURN();
 		}
-		else
-		{
-			LEDC14_OFF();
-		}
-
+		
 	}
 }
